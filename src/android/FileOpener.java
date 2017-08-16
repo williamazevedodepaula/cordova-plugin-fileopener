0b65038037d3cc86d063ff52a1c5ccbc0f11846e
william <william@adamantio.com.br>
2017-08-16 00:32:47 -0300

    using correct array size function

--- src/android/FileOpener.java
+++ src/android/FileOpener.java
@@ -84,7 +84,7 @@ public class FileOpener extends CordovaPlugin {
             if (extension != null) {
                 String  fileURL = args.getString(0);
                 boolean cached = true;
-                if (args.size() > 0){
+                if (args.length() > 1){
                     cached = args.getBoolean(1);
                 }
                 if (fileURL.startsWith("file://")) {
